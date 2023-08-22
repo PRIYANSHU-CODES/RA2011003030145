1c98d24254fb5c63d18a68449a0770cdd3f70e04
PRIYNSHU-CODES <pk1717@srmist.edu.in>
2023-08-22 15:47:51 +0530

    added folder

---
+++ Task1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className = "first">
+          <h1 className ="second">Testing1</h1>
+        </div>
+      </header>
+    </div>  
+  );
+}
+
+export default App;
